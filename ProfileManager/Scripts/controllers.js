7ca5c788c7a08dfa6cf39ba7480d465ecabe60c1
gkar68 <ghassan.karwchan@gmail.com>
2013-12-24 00:20:40 -0700

    m

---
+++ ProfileManager/Scripts/controllers.js
@@ -0,0 +1,100 @@
+﻿metadataManager.controller('profileController', function ($scope, profileFactory) {
+    $scope.myProfiles = [];
+
+    function init() {
+        $scope.myProfiles = profileFactory.query();
+    }
+
+    init();
+});
+
+
+metadataManager.controller('serviceController', function ($scope, mapServiceFactory) {
+    $scope.mapServices = [];
+
+    function init() {
+        $scope.mapServices = mapServiceFactory.query();
+    }
+
+    init();
+});
+
+
+
+metadataManager.controller('profileDetailController', function ($scope, $stateParams, profileFactory) {
+    $scope.currentProfile = {};
+    function init() {
+        $scope.currentProfile = profileFactory.get({ Id: $stateParams.profileId });
+    }
+
+    init();
+});
+
+
+metadataManager.controller('profileDetailMapServiceController', function ($scope, $stateParams, profileServiceFactory) {
+    $scope.profileMapServices = [];
+    function init() {
+        $scope.profileMapServices = profileServiceFactory.query({ Id: $stateParams.profileId });
+    }
+
+    init();
+});
+
+
+
+metadataManager.controller('profileDetailAdController', function ($scope, $stateParams, profileAdFactory) {
+    $scope.profileAdGroups = [];
+    function init() {
+        $scope.profileAdGroups = profileAdFactory.query({ Id: $stateParams.profileId });
+    }
+
+    init();
+});
+
+
+metadataManager.controller('profileEntitiesController', function ($scope, $stateParams, profileEntitiesFactory) {
+    $scope.profileEntities = [];
+    function init() {
+        $scope.profileEntities = profileEntitiesFactory.query({ Id: $stateParams.profileId });
+    }
+
+    init();
+});
+
+metadataManager.controller('serviceDetailController', function ($scope, $stateParams, mapServiceFactory) {
+    $scope.currentService = {};
+    function init() {
+        $scope.currentService = mapServiceFactory.get({ Id: $stateParams.serviceId });
+    }
+
+    init();
+});
+
+metadataManager.controller('serviceDetailArcgisController', function ($scope, $stateParams, mapServiceFactory) {
+    $scope.currentService = {};
+    $scope.getLiveData = getLiveDataFunc;
+
+
+    function getLiveDataFunc() {
+        var url = "http://dapp03080/arcgis" + $scope.currentService.Url;
+        window.open(url);
+    }
+
+    function init() {
+        $scope.currentService = mapServiceFactory.get({ Id: $stateParams.serviceId });
+    }
+
+    init();
+
+});
+
+
+
+metadataManager.controller('serviceDetailLayersController', function ($scope, $stateParams, serviceLayerFactory) {
+    $scope.serviceLayers = [];
+    function init() {
+        $scope.serviceLayers = serviceLayerFactory.query({ Id: $stateParams.serviceId });
+    }
+
+    init();
+});
---
+++ ProfileManager/Scripts/dataProviders.js
@@ -0,0 +1,25 @@
+﻿metadataManager.service('profileFactory', function ($resource) {
+    return $resource("api/profiles/:Id", { Id: "@Id" }, { "update": { method: "PUT" } });
+});
+
+metadataManager.service('mapServiceFactory', function ($resource) {
+    return $resource("api/services/:Id", { Id: "@Id" }, { "update": { method: "PUT" } });
+});
+
+
+metadataManager.service('profileServiceFactory', function ($resource) {
+    return $resource("api/profiles/:Id/mapServices", { Id: "@Id" }, { "update": { method: "PUT" } });
+});
+
+metadataManager.service('profileAdFactory', function ($resource) {
+    return $resource("api/profiles/:Id/adGroups", { Id: "@Id" }, { "update": { method: "PUT" } });
+});
+
+metadataManager.service('profileEntitiesFactory', function ($resource) {
+    return $resource("api/profiles/:Id/searchEntities", { Id: "@Id" }, { "update": { method: "PUT" } });
+});
+
+
+metadataManager.service('serviceLayerFactory', function ($resource) {
+    return $resource("api/services/:Id/layers", { Id: "@Id" }, { "update": { method: "PUT" } });
+});
